1cf550e024347f5dedb6e654366c1aa77de90481
Daniel Zehe <mail@daniel-zehe.de>
2016-09-27 22:48:29 +0800

    Modified: - added comments to the major api calls (guests,guest...) 		- return the right error code in case items cannot be found 		- implemented the api call to add a new guest

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ var url = 'mongodb://rsvp_user:rsvp_user@localhost:27017/rsvp_engine';
 
 var db = null;
 
+/*
+*	This makes sure that we are connected to the db before doing any api call.
+*/
 app.use(function(req,res,next){
 	if(db==null){
 		MongoClient.connect(url, function(err,_db){
@@ -41,43 +44,91 @@ app.get('/',function(req,res){
 api_router.get('/',function(req,res){
 	res.status(200).send("all API Endpoints");
 });
-
+/*
+*  returns a JSON array of all guests. This will return the data as is in the database, maybe processing should be done.
+*/
 api_router.get('/guests',function(req,res){
 	db.collection('guests').find({}).toArray(function(err,guests){
-		if(!err)
+		if(!err) {
 			res.status(200).send(guests);
+		}
+		else {
+			res.status(500).send();
+		}
 	});
 });
-//this id is the mongodb objectID
+
+/*
+*  returns a specific guest as given by the :id parameter.
+*  The id is the string representation of the _id in the mongoDB object.
+*/
 api_router.get('/guest/id/:id',function(req,res){
 	var guest_objectid = new ObjectID(req.params.id);
 	db.collection('guests').findOne({_id:guest_objectid},function(err,guest){
-		if(!err)
-			res.status(200).send(guest);
+		if(!err){
+			if(guest == null){
+				res.status(404).send();
+			}	
+			else {
+				res.status(200).send(guest);
+			}
+		}
+		else {
+			res.status(500).send();
+		}
 	});
 });
 
-//this id is base58
+/*
+*  returns a specific guest as given by the :id parameter.
+*  The id is a base58 encoded number that is referenced in the personID field of the guest JSON object. 
+*/
 api_router.get('/guest/personID/:id',function(req,res){
-	var guest_personID = Base58.decode(req.params.id);
+	var guest_personID = req.params.id;
 	console.log("personID (b58/int): "+req.params.id+"/"+guest_personID);
 
 	db.collection('guests').findOne({personID:guest_personID},function(err,guest){
 		if(!err){
-			console.log(guest)
-			res.status(200).send(guest);
+			console.log(guest);
+			
+			if(guest == null){
+				res.status(404).send();
+			}	
+			else {
+				res.status(200).send(guest);
+			}
+		}
+		else  {
+			res.status(500).send();
 		}
 	});
-
 });
 
-// api_router.put('/guest',function(req,res){
-// 	var guest = req.body;
+/*
+*	adds a new guest object to the database. Before adding, the latest personID is requested from the database
+*	and increased by one. Possible point of failure when high demand, should be atomic.	
+*/
 
-	
+api_router.put('/guest',function(req,res){
+	var guest = req.body;
 
-// 	res.status(200).send();
-// });
+	 db.collection('guests').find({},{_id:0,personID:1}).sort({personID:-1}).limit(1).toArray(function(err,personID){
+	 	var nextpersonID = 10000;
+	 	if(Object.getOwnPropertyNames(personID[0]).length>0){
+	 		nextpersonID = Base58.decode(personID[0].personID)+1;
+	 	}
+
+	 	guest.personID = Base58.encode(nextpersonID); 
+	 	db.collection('guests').insert(guest,function(err,result){
+	 		if(!err){
+	 			res.status(200).send();
+	 		}
+	 		else{
+	 			res.status(500).send();
+	 		}
+	 	});
+	 });
+});
 
 
 
